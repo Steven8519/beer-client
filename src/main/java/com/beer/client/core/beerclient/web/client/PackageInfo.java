7aa9b20ab73e6d4027476f207543be89268925cc
steven <engineers2526@gmail.com>
2021-07-26 09:38:41 -0500

    Initial commit

---
+++ src/main/java/com/beer/client/core/beerclient/web/client/PackageInfo.java
@@ -0,0 +1,1 @@
+package com.beer.client.core.beerclient.web.client;
---
+++ src/test/java/com/beer/client/core/beerclient/web/client/BreweryClientTest.java
@@ -0,0 +1,12 @@
+package com.beer.client.core.beerclient.web.client;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BreweryClientTest {
+
+    @Test
+    void getBeerById() {
+    }
+}
